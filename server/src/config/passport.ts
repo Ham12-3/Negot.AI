14848e6c24058dd80d0d7f0cbe1d57714a82281c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-10-29 21:15:32 +0000

    update

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,48 +1,44 @@
-import passport from "passport"
+import passport from "passport";
 
-import {Strategy as GoogleStrategy} from "passport-google-oauth20"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
-import User, {IUser} from "../models/user.model"
-import dotenv from 'dotenv';
+import User, { IUser } from "../models/user.model";
+import dotenv from "dotenv";
 dotenv.config(); // This should be at the top of your file (or in your main server file)
 
-
-
-
 passport.use(
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL: "/auth/google/callback",  // Absolute URL
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`, // Absolute URL
     },
     async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ googleId: profile.id });
-            if (!user) {
-                user = await User.create({
-                    googleId: profile.id,
-                    email: profile.emails![0].value,
-                    displayName: profile.displayName,
-                    profilePicture: profile.photos![0].value
-                });
-            }
-
-            done(null, user);
-        } catch (error) {
-            done(error as Error, undefined);
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            email: profile.emails![0].value,
+            displayName: profile.displayName,
+            profilePicture: profile.photos![0].value,
+          });
         }
-    })
+
+        done(null, user);
+      } catch (error) {
+        done(error as Error, undefined);
+      }
+    }
+  )
 );
 
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
 
+passport.deserializeUser(async (id: string, done) => {
+  const user = await User.findById(id);
 
-passport.serializeUser((user:any, done)=> {
-    done(null, user.id)
-})
-
-
-passport.deserializeUser(async(id: string,done)=> {
-    const user = await User.findById(id)
-
-    done(null, user)
-})
+  done(null, user);
+});
