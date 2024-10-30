42d2f9ef36de79ebbf4abeb5e4c11fed14fb3155
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-10-30 01:17:52 +0000

    update

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/auth/google/callback", // Absolute URL
+      callbackURL: "https://negot-ai.onrender.com/auth/google/callback", // Absolute URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
